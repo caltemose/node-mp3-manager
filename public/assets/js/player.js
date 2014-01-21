60ba87890c95a1af35e2bc1b7fddac47afd090cc
Chad Altemose <chad@chadzilla.com>
2014-01-21 07:53:29 -0500

    player playlist functionality in-progress. need to update playlist creation to allow for NAS vs local playlists.

--- public/assets/js/player.js
+++ public/assets/js/player.js
@@ -7,13 +7,28 @@ var player = {
     player.music = data;
     player.currentAlbum = 0;
     player.currentTrack = 0;
-    player.play();
+
+    var playlist = player.audioElement.attr('data-playlist');
+    if (playlist) {
+      //load playlist
+      $.get(playlist, function(data) {
+        console.log(data);
+      });
+    } else
+      player.play();
   },
 
   play: function() {
     console.log('player.play()');
-    var track = player.music[player.currentAlbum].tracks[player.currentTrack];
-    player.audioElement.attr('src', track.path).get(0).play();
+    var track, path;
+    if (player.playlist) {
+      path = player.playlist[player.currentTrack];
+      console.log('playlist path: ' + path);
+    } else {
+      track = player.music[player.currentAlbum].tracks[player.currentTrack];
+      path = track.path;
+    }
+    player.audioElement.attr('src', path).get(0).play();
     player.audioElement.bind('ended', player.playEnded);
   },
 
@@ -24,17 +39,27 @@ var player = {
 
   playNext: function() {
     console.log('player.playNext()');
-    var album = player.music[player.currentAlbum];
-    if (player.currentTrack < album.tracks.length -1) 
-      player.currentTrack++;  
-    else {
-      player.currentTrack = 0;
-      //comment out to loop an album
-      if (player.currentAlbum < player.music.length-1) 
-        player.currentAlbum++;
-      else player.currentAlbum = 0;
+    if (player.playlist) {
+      if (player.currentTrack < player.playlist.length-1) {
+        player.currentTrack++;
+        player.play();
+      } else {
+        console.log("Done playing playlist.");
+      }
+    } else {
+      var album = player.music[player.currentAlbum];
+      if (player.currentTrack < album.tracks.length -1) 
+        player.currentTrack++;  
+      else {
+        player.currentTrack = 0;
+        //comment out to loop an album
+        if (player.currentAlbum < player.music.length-1) 
+          player.currentAlbum++;
+        else player.currentAlbum = 0;
+      }
+      player.play();
     }
-    player.play();
+    
   }
 };
 
--- routes/player.js
+++ routes/player.js
@@ -2,7 +2,7 @@ module.exports = function(nmm) {
   return {
     play: function(req, res) {
       var data = {};
-      if (req.query.playlist) data.playlist = req.query.playlist;
+      if (req.query.id) data.id = req.query.id;
       res.render('player', data);
     }
   }
